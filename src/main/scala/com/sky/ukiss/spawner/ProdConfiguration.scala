c133bf3d8488dfc80a0de115134957191a7bff2f
hermes <hermes@bskyb.com>
2018-01-15 16:52:30 +0000

    added prometheus metrics

--- src/main/scala/com/sky/ukiss/spawner/ProdConfiguration.scala
+++ src/main/scala/com/sky/ukiss/spawner/ProdConfiguration.scala
@@ -1,9 +1,15 @@
 package com.sky.ukiss.spawner
 
+import io.prometheus.client.CollectorRegistry
+import io.prometheus.client.hotspot.DefaultExports
 import net.liftweb.util.SimpleInjector
 import sky.sns.hermes.status.service.{StatusService, StatusServiceConfig, StatusServiceImpl}
 
 object ProdConfiguration extends SimpleInjector {
   val statusServiceConfig = new Inject[StatusServiceConfig](() => new StatusServiceConfig.Builder("0.0.1").build()) {}
   val statusService = new Inject[StatusService](() => new StatusServiceImpl(statusServiceConfig.vend)) {}
+  val collectorRegistry = new Inject[CollectorRegistry](() => {
+    DefaultExports.initialize()
+    CollectorRegistry.defaultRegistry
+  }){}
 }
--- src/main/scala/com/sky/ukiss/spawner/krr/Metrics.scala
+++ src/main/scala/com/sky/ukiss/spawner/krr/Metrics.scala
@@ -1,10 +1,18 @@
 package com.sky.ukiss.spawner.krr
 
+import java.io.StringWriter
+import javax.servlet.http.HttpServletResponse
+
+import com.sky.ukiss.spawner.ProdConfiguration
+import io.prometheus.client.exporter.common.TextFormat
+import net.liftweb.http.PlainTextResponse
 import net.liftweb.http.rest.RestHelper
-import net.liftweb.json.JsonAST.JObject
 
 object Metrics extends RestHelper {
   serve {
-    case Get("metrics" :: Nil, _) => JObject()
+    case Get("metrics" :: Nil, _) =>
+      val writer = new StringWriter()
+      TextFormat.write004(writer, ProdConfiguration.collectorRegistry.vend.metricFamilySamples())
+      PlainTextResponse(writer.toString, List("Content-Type" -> TextFormat.CONTENT_TYPE_004), HttpServletResponse.SC_OK)
   }
 }
