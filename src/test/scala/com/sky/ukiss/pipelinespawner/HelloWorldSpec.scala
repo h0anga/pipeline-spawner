6f79975576c95553cc408869bec52ca6a6247ece
Sonique <team.sonique.sns@bskyb.com>
2018-03-07 15:54:00 +0000

    wow, I can test! And found an integration problem with Kubernetes! AWESOME!!!^

--- src/test/scala/com/sky/ukiss/pipelinespawner/HelloWorldSpec.scala
+++ src/test/scala/com/sky/ukiss/pipelinespawner/HelloWorldSpec.scala
@@ -1,23 +1,20 @@
 package com.sky.ukiss.pipelinespawner
 
-import java.io.{BufferedReader, InputStreamReader}
-
 import cats.effect.IO
 import org.http4s._
 import org.specs2.matcher.MatchResult
+import org.specs2.mutable._
 
 import scala.io.Source
 
-class HelloWorldSpec extends org.specs2.mutable.Specification {
-  val hookJson: String = Source.fromResource("/git-hook.json").mkString
+class HelloWorldSpec extends Specification {
+  lazy val hookJson: String = Source.fromResource("git-hook.json").mkString
 
   def gitHookEndpoint = new Configuration().gitHookServiceComponent.service
 
   "HelloWorld" >> {
     "return 200" >> {
-      val request = Request[IO](Method.GET, Uri.uri("/hello/world"))
-      val response = HelloWorldServer.helloService(request)
-      response === """{"message":hello, world"}"""
+      uriReturns200()
     }
 
     "return hello world" >> {
@@ -25,31 +22,33 @@ class HelloWorldSpec extends org.specs2.mutable.Specification {
     }
   }
 
-  import fs2.Stream
-  import cats.effect.IO._
-
-  "Git Hook" >> {
-    "return 200" >> {
-      val post = Request(Method.POST, Uri.uri("/hook")).withBody(hookJson).unsafeRunSync()
-      val task = gitHookEndpoint.run(post)
-      val response = ??? // TODO get the response
-    }
-
-    "return the id of the job spawned" >> {
-
-    }
+  private[this] val retHelloWorld: Response[IO] = {
+    val request = Request[IO](Method.GET, Uri.uri("/hello/world"))
+    val response: Response[IO] = HelloWorldServer.helloService(request).getOrElse(???).unsafeRunSync()
+    response
   }
-
-//  private[this] val retHelloWorld: Response[IO] = {
-//    val request = Request[IO](Method.GET, Uri.uri("/hello/world"))
-//    val responseTask = HelloWorldServer.helloService.run(request)
-//    val response: Option[_] = responseTask.run.body.runLast.run
-//    response.get.decodeUtf8.right.get shouldEqual """{"message":hello, world"}"""
-//  }
 
   private[this] def uriReturns200(): MatchResult[Status] =
     retHelloWorld.status must beEqualTo(Status.Ok)
 
   private[this] def uriReturnsHelloWorld(): MatchResult[String] =
     retHelloWorld.as[String].unsafeRunSync() must beEqualTo("{\"message\":\"Hello, world\"}")
+
+  import cats.effect.IO._
+
+  "Git Hook" >> {
+    "return 200" >> {
+      retGitHook.status must_== Status.Ok
+    }
+
+    "return the id of the job spawned" >> {
+      retGitHook.as[String].unsafeRunSync() must contain("the id of the job")
+    }
+  }
+
+  private lazy val retGitHook: Response[IO] = {
+    val post = Request(Method.POST, Uri.uri("/hook")).withBody(hookJson).unsafeRunSync()
+    gitHookEndpoint(post).getOrElse(???).unsafeRunSync()
+  }
+
 }
