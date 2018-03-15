4a940aaad55f7f2bfd2b06641d7dff13861a9a7a
Sonique <team.sonique.sns@bskyb.com>
2018-03-15 09:10:47 +0000

    make Config a bit more scala-esque

--- src/main/scala/com/sky/ukiss/pipelinespawner/Config.scala
+++ src/main/scala/com/sky/ukiss/pipelinespawner/Config.scala
@@ -3,7 +3,7 @@ package com.sky.ukiss.pipelinespawner
 import com.typesafe.config._
 
 object Config {
-  val env = if (System.getenv("ENVIRONMENT") == null) "development" else System.getenv("ENVIRONMENT")
+  val env = sys.env.getOrElse("ENVIRONMENT", "development")
 
   val conf = ConfigFactory.load()
   def apply() = conf.getConfig(env)
