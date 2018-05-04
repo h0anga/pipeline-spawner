0ef36633e5b5e3ce02b62808069e5e41ca943100
imke.himstedt <imke.himstedt@sky.uk>
2018-05-04 09:21:01 +0100

    re-comment-out Server

--- backend/src/main/scala/com/sky/ukiss/pipelinespawner/Server.scala
+++ backend/src/main/scala/com/sky/ukiss/pipelinespawner/Server.scala
@@ -1,21 +1,15 @@
 package com.sky.ukiss.pipelinespawner
 
-import cats.effect.IO
-import fs2.StreamApp
-import org.http4s.dsl.Http4sDsl
-import org.http4s.server.blaze.BlazeBuilder
-
-import scala.concurrent.ExecutionContext.Implicits.global
-
-object Server extends StreamApp[IO] with Http4sDsl[IO] {
-
-  def stream(args: List[String], requestShutdown: IO[Unit]) = {
-    val context = new Context
-    BlazeBuilder[IO]
-      .bindHttp(8080, "0.0.0.0")
-      .mountService(context.gitHookServiceComponent.service, "/")
-      .mountService(context.frontendRoute.service, "/")
-      .serve
-  }
-
-}
+//object Server extends StreamApp[IO] with Http4sDsl[IO] {
+//
+//  def stream(args: List[String], requestShutdown: IO[Unit]) = {
+//    val context = new Context
+//    BlazeBuilder[IO]
+//      .bindHttp(8080, "0.0.0.0")
+//      .mountService(context.gitHookServiceComponent.service, "/")
+//      .mountService(context.frontendRoute.service, "/")
+//      .serve
+//  }
+//
+//}
+//
