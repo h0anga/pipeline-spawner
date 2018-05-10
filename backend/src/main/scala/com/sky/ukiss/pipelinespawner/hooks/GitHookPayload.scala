35dcebdf6a14ad911cc0b9dc3d9b47857348566f
Sonique <team.sonique.sns@bskyb.com>
2018-05-10 16:19:00 +0100

    [alb] add forgotten file

---
+++ backend/src/main/scala/com/sky/ukiss/pipelinespawner/hooks/GitHookPayload.scala
@@ -0,0 +1,6 @@
+package com.sky.ukiss.pipelinespawner.hooks
+
+case class GitHookPayload(user_name: String, project: Project, commits: List[Commit], after: String, total_commits_count: Int)
+case class Project(id: Long, name: String, web_url: String, description: String, git_ssh_url: String, git_http_url: String, namespace: String, homepage: String)
+case class Commit(id: String, message: String, url: String, author: Author)
+case class Author(name: String, email: String)
