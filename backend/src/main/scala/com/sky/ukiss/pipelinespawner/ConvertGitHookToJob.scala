9fbeb032511ffb3696c636fadf43916e26b9eff9
imke.himstedt <imke.himstedt@sky.uk>
2018-05-22 11:06:45 +0100

    [alb, Oksana] make build and push image work for simba-forked

--- backend/src/main/scala/com/sky/ukiss/pipelinespawner/ConvertGitHookToJob.scala
+++ backend/src/main/scala/com/sky/ukiss/pipelinespawner/ConvertGitHookToJob.scala
@@ -50,7 +50,7 @@ spec:
         command:
         - bash
         - -c
-        - git clone $cloneUrl application && cd application/pipeline && git checkout $commit && make build push
+        - sudo chmod a+rw /var/run/docker.sock && git clone $cloneUrl application && cd application/pipeline && git checkout $commit && make push
         env:
         - name: ARTIFACTORY_USERNAME
           valueFrom:
@@ -64,22 +64,72 @@ spec:
               key: artifactoryUser
         - name: GO_PIPELINE_LABEL
           value: "$now"
-        - name: DOCKER_HOST
-          value: tcp://pipeline-spawner-dind-daemon:2375
         volumeMounts:
         - name: secret-volume
           mountPath: /build/.ssh
-        - name: docker-exec-volume
-          mountPath: /usr/bin/docker
+        - name: docker-exec-location
+          mountPath: /usr/local/bin
+        - name: build-dir
+          mountPath: /build/application
+        - name: var-run
+          mountPath: /var/run
+      - name: dind-daemon
+        image: docker:18.05.0-dind
+        args: ["--insecure-registry", "repo.sns.sky.com:8186", "--insecure-registry", "repo.sns.sky.com:8185", "-D"]
+        resources:
+          requests:
+            cpu: 20m
+            memory: 512Mi
+        lifecycle:
+          postStart:
+            exec:
+              command: [cp, /usr/local/bin/docker, /var/run/pipeline-spawner/docker]
+        securityContext:
+          privileged: true
+        volumeMounts:
+          - name: docker-graph-storage
+            mountPath: /var/lib/docker
+          - name: docker-exec-location
+            mountPath: /var/run/pipeline-spawner
+          - name: plugins
+            mountPath: /run/docker/plugins/
+          - name: json-store
+            mountPath: /var/lib/docker/plugin-data/
+          - name: data-volume
+            mountPath: /data
+          - name: var-run
+            mountPath: /var/run
+      - name: local-volume-plugin
+        image: cwspear/docker-local-persist-volume-plugin:v1.3.0
+        volumeMounts:
+          - name: plugins
+            mountPath: /run/docker/plugins/
+          - name: json-store
+            mountPath: /var/lib/docker/plugin-data/
+          - name: data-volume
+            mountPath: /data
       volumes:
       - name: secret-volume
         secret:
           defaultMode: 420
           secretName: pipeline-spawner-secret
-      - name: docker-exec-volume
-        hostPath:
-          path: /var/run/pipeline-spawner/docker
+      - name: docker-graph-storage
+        emptyDir: {}
+      - name: plugins
+        emptyDir: {}
+      - name: json-store
+        emptyDir: {}
+      - name: data-volume
+        emptyDir: {}
+      - name: docker-exec-location
+        emptyDir: {}
+      - name: build-dir
+        emptyDir: {}
+      - name: var-run
+        emptyDir: {}
       """
+
+    println(jobYaml)
 
     kubernetesClient.extensions().jobs().load(new ByteArrayInputStream(jobYaml.getBytes)).get()
   }
